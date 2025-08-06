76529b249b44bb3b52d62f306fca8c67e499eabc
user <user@users-MacBook-Pro.local>
2025-08-06 14:38:17 +0100

    update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,6 +6,7 @@ import PopularGames from '../components/PopularGames/PopularGames';
 // import TakeQuiz from '../components/QuizSection/TakeQuiz';
 // import UpcomingEvent from '../components/UpcomingEvents/UpcomingEvent';
 import Footer from '../components/Footer/Footer';
+import { Link } from 'react-router-dom';
 
 
 
@@ -16,10 +17,12 @@ export default function Home(){
             <HeroSection/>
 
             <div className="relative -mt-12 flex justify-center gap-6">
-                <Card icon={<FaBookOpen />} title="Bible Stories" bgColor="bg-indigo-700" onClick={() => navigate("/bible-stories")} />
-                <Card icon={<FaGamepad />} title="Games" bgColor="bg-purple-600" onClick={() => navigate("/games")} />
-                <Card icon={<FaQuestionCircle />} title="Quiz" bgColor="bg-blue-600" onClick={() => navigate("/quiz")} />
-                <Card icon={<FaCalendarAlt />} title="Schedule" bgColor="bg-teal-600" onClick={() => navigate("/schedule")} />
+                <Link to="/bible-stories"><Card icon={<FaBookOpen />} title="Bible Stories" bgColor="bg-indigo-700" onClick={() => navigate("/bible-stories")} /></Link>  
+                <Link to="/games"><Card icon={<FaGamepad />} title="Games" bgColor="bg-purple-600" onClick={() => navigate("/games")} /></Link>
+                <Link to="/quiz"><Card icon={<FaQuestionCircle />} title="Quiz" bgColor="bg-blue-600" onClick={() => navigate("/quiz")} /></Link>
+                <Link to="/schedule"><Card icon={<FaCalendarAlt />} title="Schedule" bgColor="bg-teal-600" onClick={() => navigate("/schedule")} /></Link>
+                
+                
             </div>
             
             <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8">
